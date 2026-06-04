49744696e49c91f4a2c91fb204a86ceda94db46f
Tromtrevor <traitaes@gmail.com>
2026-06-04 07:27:53 +0300

    modified run.py

--- backend/run.py
+++ backend/run.py
@@ -3,5 +3,5 @@ import uvicorn
 
 if __name__ == "__main__":
     # Render automatically provides a PORT environment variable
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8001))
     uvicorn.run("api.main:app", host="0.0.0.0", port=port)
